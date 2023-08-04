d102bb7ce95d0a0d66421a3b68b08ddad00897d7
Sonbeomgyu1 <sjk6285@naver.com>
2023-08-04 11:54:04 +0900

    .

---
+++ 20230721_jdbc_kh/src/main/java/kh/test/jdbckh/ajax/controller/AjaxPageController.java
@@ -0,0 +1,30 @@
+package kh.test.jdbckh.ajax.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AjaxPageController
+ */
+@WebServlet("/ajaxtest")
+public class AjaxPageController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public AjaxPageController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/view/ajaxtest.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
